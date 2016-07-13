88e1e8d192c896a13842c707b823b5086a85bec4
Jaakko Kantojärvi <jaakko@n-1.fi>
2016-07-13 20:55:01 +0300

    Reimplement nested routes and move lookup config to viewsets
    This change requires that parent viewset defines lookup_url_kwarg or
    lookup_field. In exchange we do not need to define anything about url kwargs at
    register time. Old way is still supported, though pending deprecation.
    
    Also there is NestedHyperlinkedRelatedField and NestedHyperlinkedIdentityField
    which will use our config from viewsets to make writing serializers simpler and
    less error prone.
    
    You can also use register using with-statement.
    
    Co-authored-by: Markku Riekkinen <markku.riekkinen@aalto.fi>

--- rest_framework_extensions/mixins.py
+++ rest_framework_extensions/mixins.py
@@ -1,8 +1,16 @@
+import logging
+
+from django.core.exceptions import FieldError
+from django.http import Http404
+from django.utils.encoding import force_str
+
 from rest_framework_extensions.cache.mixins import CacheResponseMixin
 # from rest_framework_extensions.etag.mixins import ReadOnlyETAGMixin, ETAGMixin
 from rest_framework_extensions.bulk_operations.mixins import ListUpdateModelMixin, ListDestroyModelMixin
 from rest_framework_extensions.settings import extensions_api_settings
-from django.http import Http404
+
+
+logger = logging.getLogger('rest_framework_extensions.request')
 
 
 class DetailSerializerMixin:
@@ -50,22 +58,50 @@ class PaginateByMaxMixin:
 
 
 class NestedViewSetMixin:
+    """
+    Adds filtering in get_page_size based on .parent_lookup_map definitions.
+
+    Raises:
+        Http404: If queryset.filter() raised ValueError.
+            This happens if filter string was wrong.
+    """
+
     def get_queryset(self):
         return self.filter_queryset_by_parents_lookups(
             super().get_queryset()
         )
 
+    def filter_queryset(self, queryset):
+        queryset = self.filter_queryset_by_parents_lookups(queryset)
+        return super().filter_queryset(queryset)
+
     def filter_queryset_by_parents_lookups(self, queryset):
-        parents_query_dict = self.get_parents_query_dict()
-        if parents_query_dict:
+        """
+        Filter queryset using parent_lookup_map
+        """
+        map_ = getattr(self, 'parent_lookup_map', {})
+        filters = self.get_parents_query_dict() # TODO: replace with {} when call is removed
+        for kw, filter_ in map_.items():
+            if callable(filter_):
+                filter_ = filter_()
+            filter_ = force_str(filter_).replace('.', '__')
+            value = self.kwargs.get(kw, None)
+            if value is not None:
+                filters[filter_] = value
+        if filters:
             try:
-                return queryset.filter(**parents_query_dict)
-            except ValueError:
+                return queryset.filter(**filters)
+            except (ValueError, FieldError):
+                logger.exception("queryset filtering with parent_lookup_map failed")
                 raise Http404
         else:
             return queryset
 
     def get_parents_query_dict(self):
+        """
+        Resolve legacy parent query dict.
+        Deprecated in the development version. Warning is raised in NestedRouterItem if this feature is used.
+        """
         result = {}
         for kwarg_name, kwarg_value in self.kwargs.items():
             if kwarg_name.startswith(extensions_api_settings.DEFAULT_PARENT_LOOKUP_KWARG_NAME_PREFIX):
